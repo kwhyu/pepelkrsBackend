737275b35a510bbbcea345847cf9610358259e02
Eka Wahyu <ekawahyu9311@gmail.com>
2024-11-11 23:55:24 +0800

    Fix dotenv issue for Railway deployment

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mahasiswaRoutes = require('./routes/mahasiswaRoutes');
+const matakuliahRoutes = require('./routes/matakuliahRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Selamat Datang');
+});
+
+// Register routes
+app.use('/mahasiswa', mahasiswaRoutes);
+app.use('/matakuliah', matakuliahRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
